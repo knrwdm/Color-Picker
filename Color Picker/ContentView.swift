f1fef4c0371a3021114fc63e416d5a18673abef5
Kayla Reap <kaylareap@Kaylas-MacBook-Pro.local>
2019-10-11 13:41:47 -0500

    first commit

---
+++ Color Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Color Picker
+//
+//  Created by Kayla Reap on 10/11/19.
+//  Copyright © 2019 Kayla Reap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

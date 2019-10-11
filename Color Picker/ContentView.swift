34306bfe0e9d3351ebcada60cb19dce205d2a461
Kayla Reap <kaylareap@Kaylas-MacBook-Pro.local>
2019-10-11 14:15:05 -0500

    Tried to add color to label

--- Color Picker/ContentView.swift
+++ Color Picker/ContentView.swift
@@ -6,16 +6,35 @@
 //  Copyright © 2019 Kayla Reap. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+class ContentView: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let colors =["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = colors[row]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 }
